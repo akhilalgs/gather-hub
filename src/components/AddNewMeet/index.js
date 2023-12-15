025db4b669d5ffbbca57b1f7c4f994dba46fe9b1
admin <jalamni55@gmil.com>
2023-12-15 16:32:58 +0530

    code changed

--- src/components/AddNewMeet/index.js
+++ src/components/AddNewMeet/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../Navbar";
 import { FaRegCalendarAlt } from "react-icons/fa";
-import { MdAccessTimeFilled } from "react-icons/md";
+import { FaRegClock } from "react-icons/fa";
 import DatePicker from "react-datepicker";
 import TimePicker from "react-time-picker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,61 +9,45 @@ import "react-time-picker/dist/TimePicker.css";
 import { IoPeople } from "react-icons/io5";
 import { CgNotes } from "react-icons/cg";
 import "./index.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom/dist";
+import it from "react-dom/client"
 
 const AddNewMeet = () => {
-    const [addNewMeet, setAddNewMeet] = useState({
-        title: "",
-        description: "",
-        meeting_date: new Date(),
-        start_time: "",
-        end_time: "",
-        meeting_attendees: "",
-        
-    });
+  
+    const[title,setTitle] = useState("")
+    const[description,setDescription] = useState("")
+    const[meeting_date,setMeeting_date] = useState("")
+    const[start_time,setStart_time] = useState("")
+    const[end_time,setEnd_time] = useState("")
+    const [meetingAttendees, setMeetingAttendees] = useState([]);
+    const navigate = useNavigate();
 
-    const handleDateChange = (date) => {
-        setAddNewMeet({ ...addNewMeet, meeting_date: date });
-    };
-
-    const handleStartTimeChange = (start_time) => {
-        setAddNewMeet({ ...addNewMeet, start_time });
-    };
-
-    const handleEndTimeChange = (end_time) => {
-        setAddNewMeet({ ...addNewMeet, end_time });
-    };
-
-    
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setAddNewMeet({ ...addNewMeet, [name]: value });
-    };
-    const handleSubmit = async (e) => {
+    const save = async (e) => {
         e.preventDefault();
-
-        try {
-            const response = await fetch("http://localhost:8080/add", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(addNewMeet),
+        if (title === '' || description === '' || meeting_date === '' || start_time === '' || end_time === '' || meetingAttendees === '') {
+    
+            alert('Please fill in all fields.');
+            return; 
+          }
+          try {
+            const response = await axios.post('http://localhost:8080/add', {
+              title :title,
+              description:description,
+              meeting_date:meeting_date,
+              start_time:start_time,
+              end_time:end_time,
+              meeting_attendees:meetingAttendees
             });
+            alert("Metting added successfully");
+            navigate("/meetings");
+          } catch (error) {
+            console.error(error);
+          }
+                    
+       
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            // If the response is successful, you can handle it here
-            console.log('Form submitted successfully');
-            const data = response.json()
-            console.log(data)
-
-            // Reset the form or redirect if needed
-        } catch (error) {
-            console.error('Error submitting form:', error);
-            // Handle errors, maybe show an error message to the user
-        }
+           
     };
 
 
@@ -71,32 +55,40 @@ const AddNewMeet = () => {
         <>
             <Navbar />
             <div>
-                <form onSubmit={handleSubmit}>
-                    <input className="Meeting-heading" placeholder="Add Title" type="text" value={addNewMeet.title}
-                            onChange={handleInputChange}/>
+                <form >
+                <input
+    className="Meeting-heading"
+    placeholder="Add Title"
+    type="text"
+    name="title"
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}
+/>
+
                     <hr className="Meeting-underline" />
                     <div className="meeting-label-container">
                         <label className="meeting-label"><FaRegCalendarAlt className="icon-size" /></label>
-                        <DatePicker className="date-picker" selected={addNewMeet.meeting_date} onChange={handleDateChange} />
+                        <DatePicker className="date-picker" selected={meeting_date} onChange={date => setMeeting_date(date)} />
                     </div >
                     <div className="meeting-label-container">
                         <IoPeople className="icon-size-1"/>
-                        <input className="date-picker meeting-label"
-                            type="text"
-                            name="add-guest"
-                            placeholder="Add Attendees / Add Guests"
-                            //value={addNewMeet.agenda}
-                            onChange={handleInputChange}/>
-                       
+                        <input
+    className="date-picker meeting-label"
+    type="text"
+    name="add-guest"
+    placeholder="Add Attendees / Add Guests (Separate with commas)"
+    value={meetingAttendees.join(',')} // Display the array as a string with commas
+    onChange={(e) => setMeetingAttendees(e.target.value.split(','))}
+/>                       
                     </div>
                     <div className="meeting-label-container">                        
                         <label className="meeting-label meeting-side-heading">start Time:</label>
-                        <TimePicker className="date-picker" value={addNewMeet.start_time} onChange={handleStartTimeChange} />
+                        <input className="date-picker" value={start_time} onChange={e => setStart_time(e.target.value)} />
                     </div>
                     
                     <div className="meeting-label-container">
                         <label className="meeting-label meeting-side-heading">End Time:</label>
-                        <TimePicker className="date-picker" value={addNewMeet.end_time} onChange={handleEndTimeChange} />
+                        <input className="date-picker" value={end_time} onChange={e => setEnd_time(e.target.value)} />
                     </div>
 
                     
@@ -104,13 +96,13 @@ const AddNewMeet = () => {
                         <CgNotes className="icon-size"/>
                         <textarea className="date-picker meeting-text-area-element"                            
                             name="description"
-                            value={addNewMeet.description}
-                            onChange={handleInputChange}
-                            placeholder="Enter agenda / Description"
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                            placeholder="Description"
                         />
                     </div>
                     <div>
-                        <button className="new-metting-button" type="submit">Create</button>
+                        <button onClick={save} className="new-metting-button" type="submit">Create</button>
                     </div>
                 </form>
             </div>
