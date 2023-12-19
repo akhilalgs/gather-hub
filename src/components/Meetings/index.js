1f3b2b4b490cc452d0b37538068e68938a6cf946
admin <jalamni55@gmil.com>
2023-12-19 10:53:34 +0530

    code

--- src/components/Meetings/index.js
+++ src/components/Meetings/index.js
@@ -1,15 +1,16 @@
 
+ 
 import Navbar from "../Navbar"
 import NewMeet from "../NewMeet"
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
-
+ 
 import "./index.css"
-
+ 
 const myMeetings = [{ id: uuidv4(), when: new Date(), who: "akhila@gamil.com", agenda: "developer testing" }, { id: uuidv4(), when: new Date(), who: "akhila@gamil.com", agenda: "developer testing" }, { id: uuidv4(), when: new Date(), who: "akhila@gamil.com", agenda: "developer testing" }, { id: 4, when: new Date(), who: "akhila@gamil.com", agenda: "developer testing" }]
-
+ 
 const Meetings = () => {
     const [meetings, setMeetings] = useState([...myMeetings])
     const navigate = useNavigate();
@@ -22,54 +23,36 @@ const Meetings = () => {
                 console.error('Error fetching meetings:', error);
             }
         };
-
+ 
         fetchData();
     }, []);
-
+ 
     const newMeetButton = () => {
         navigate("/new-metting")
     }
-
+ 
     return (
         <>
-            // <Navbar />
-            // <div className="meetings-main-container">
-            //     <div className="new-button-container">
-            //         <button onClick={newMeetButton} className="new-metting-button" type="button">New Metting</button>
-            //     </div>
-
-            //     <ul className="mettings-cards-container">
-            //         {meetings.map((meeting) => (
-            //               <div key={meeting.id} className="meeting-card">
-            //                 <h3> Tittle :{meeting.title}</h3>
-            //                 <p> Date :{meeting.meeting_date}</p>
-            //                 <p> Description :{meeting.description}</p>
-            //                 <p> Time :{meeting.start_time + "-" + meeting.end_time}</p>
-                      
-                          
-            //             </div>
-            //         ))}
-            //     </ul>
-
-            // </div>
+           
         <Navbar />
             <div className="meetings-main-container">
                 <div className="new-button-container">
                     <button onClick={newMeetButton} className="new-metting-button" type="button">New Metting</button>
                 </div>
-
-        //         <ul className="mettings-cards-container">
-        //             {meetings.map((eachItem) => (
-        //                 <NewMeet key={eachItem.id} newItem={eachItem} />
-        //             ))}
-        //         </ul>
-
+ 
+                <ul className="mettings-cards-container">
+                    {meetings.map((eachItem) => (
+                        <NewMeet key={eachItem.id} newItem={eachItem} />
+                    ))}
+                </ul>
+ 
             </div>
         </>
     )
-
-
+ 
+ 
 }
-
-
+ 
+ 
 export default Meetings
+ 
